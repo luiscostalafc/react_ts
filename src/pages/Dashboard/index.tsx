5c051cc1ce10b22918645df7b33d0ccc5b24dba2
luiscostalafc <luiscostalafc@gmail.com>
2020-07-23 18:03:40 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import {FiChevronRight} from  'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-   return <Title>Explore repositórios no Github</Title>
+   return (
+     <>
+     <img src={logoImg} alt="Github Explore" />
+     <Title>Explore repositórios no Github</Title>
+     <Form>
+     <input placeholder="Digite o nome de repositório"/>
+     <button type="submit">Pesquisar</button>
+     </Form>
+
+     <Repositories>
+       <a href="teste">
+         <img
+         src="https://avatars3.githubusercontent.com/u/46203983?s=460&u=0d2997e7bab81617e224941bcfc0aff94d10436f&v=4"
+         alt="Luis Costa"
+         />
+         <div>
+           <strong>rocketseat/unform</strong>
+           <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+         </div>
+
+         <FiChevronRight size={20}/>
+
+       </a>
+       <a href="teste">
+         <img
+         src="https://avatars3.githubusercontent.com/u/46203983?s=460&u=0d2997e7bab81617e224941bcfc0aff94d10436f&v=4"
+         alt="Luis Costa"
+         />
+         <div>
+           <strong>rocketseat/unform</strong>
+           <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+         </div>
+
+         <FiChevronRight size={20}/>
+
+       </a>
+       <a href="teste">
+         <img
+         src="https://avatars3.githubusercontent.com/u/46203983?s=460&u=0d2997e7bab81617e224941bcfc0aff94d10436f&v=4"
+         alt="Luis Costa"
+         />
+         <div>
+           <strong>rocketseat/unform</strong>
+           <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+         </div>
+
+         <FiChevronRight size={20}/>
+
+       </a>
+     </Repositories>
+     </>
+   )
 }
 
 export default Dashboard;
